9bbb5bbdc6ffd924ba12645808500326b640c075
Michael Bukachi <michaelbukachi@gmail.com>
2019-03-27 21:31:28 +0300

    docs: Configure documentation file

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import datetime
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 from setup import version as setup_version
 # -- Project information -----------------------------------------------------
