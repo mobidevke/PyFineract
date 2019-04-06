58aa48ce8e125483bda462ad62533fac2665aa25
Michael Bukachi <michaelbukachi@gmail.com>
2019-04-06 21:51:26 +0300

    fix: Correct ssl issues when making requests to the fineract instance

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,8 @@
 import requests
 
-from ssl_adapter import SSLAdapter
-
-s = requests.Session()
-s.mount('https://', SSLAdapter())
-
 
 def server_test():
-    res = s.get('https://127.0.0.1:8443/api-docs/apiLive.htm', verify=False)
+    res = requests.get('https://127.0.0.1:8443/api-docs/apiLive.htm', verify=False)
     return res.ok
 
 
--- wait.py
+++ wait.py
@@ -1,23 +1,22 @@
 import time
 
 import requests
-
-from ssl_adapter import SSLAdapter
+from requests.exceptions import SSLError
 
 timeout = 75
 
-
-s = requests.Session()
-s.mount('https://', SSLAdapter())
 
 
 def wait_for_fineract():
     seconds = 0
     while seconds < timeout:
-        res = s.get('https://127.0.0.1:8443/api-docs/apiLive.htm', verify=False)
-        if res.ok:
-            print('Available after {} seconds.'.format(seconds))
-            break
+        try:
+            res = requests.get('https://127.0.0.1:8443/api-docs/apiLive.htm', verify=False)
+            if res.ok:
+                print('Available after {} seconds.'.format(seconds))
+                break
+        except SSLError:
+            pass
 
         print('Waiting for fineract instance...')
         time.sleep(5)
