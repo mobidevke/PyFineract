434e56bcfcde7331b25616db282a38e9c36aa5e9
Michael Bukachi <michaelbukachi@gmail.com>
2019-04-06 20:20:01 +0300

    build: Add wait for endpoint script

--- wait-for-http-200.sh
+++ wait-for-http-200.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-timeout 75 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' https://localhost:8443/api-docs/apiLive.htm)" != "200" ]]; do echo ''waiting for fineract instance'' && sleep 5; done' || false
+timeout 75 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' https://127.0.0.1:8443/api-docs/apiLive.htm)" != "200" ]]; do echo ''waiting for fineract instance'' && sleep 5; done' || false
