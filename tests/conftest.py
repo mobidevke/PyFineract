fbcf50def12f2a4f7d0f64e995d897111cad65a0
Michael Bukachi <michaelbukachi@gmail.com>
2019-04-06 20:35:00 +0300

    build: Add wait for endpoint script

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,11 @@
-import socket
+import requests
 
 
-def server_test(host, port):
-
-    args = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
-    for family, socktype, proto, canonname, sockaddr in args:
-        s = socket.socket(family, socktype, proto)
-        try:
-            s.connect(sockaddr)
-        except socket.error:
-            return False
-        else:
-            s.close()
-            return True
+def server_test():
+    res = requests.get('https://127.0.0.1:8443/api-docs/apiLive.htm', verify=False)
+    return res.ok
 
 
 collect_ignore = []
-if not server_test('localhost', 8443):
+if not server_test():
     collect_ignore_glob = ['integration/*.py']
