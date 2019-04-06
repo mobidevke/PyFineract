ccf20f221b79e317ef6265e30ac454d47b971966
Michael Bukachi <michaelbukachi@gmail.com>
2019-04-06 20:47:17 +0300

    fix: integration tests
    This commit corrects python2.7 tests

--- tests/integration/test_clients.py
+++ tests/integration/test_clients.py
@@ -1,20 +1,19 @@
 import pytest
 
-from fineract import Fineract
 from fineract.objects.client import Client
 
 
-def test_get_all_clients(fineract: Fineract):
+def test_get_all_clients(fineract):
     count = len([client for client in fineract.get_clients()])
     assert count == 7
 
 
-def test_get_all_clients__with_filter(fineract: Fineract):
+def test_get_all_clients__with_filter(fineract):
     count = len([client for client in fineract.get_clients(sqlSearch='c.external_id=11111111')])
     assert count == 1
 
 
-def test_get_single_client(fineract: Fineract):
+def test_get_single_client(fineract):
     client = fineract.get_client(1)
     assert client
 
@@ -23,12 +22,12 @@ def test_get_single_client(fineract: Fineract):
     ('233717890222', True),
     ('233711111111', False),
 ])
-def test_get_client_by_phone_no(fineract: Fineract, phone_no, result):
+def test_get_client_by_phone_no(fineract, phone_no, result):
     client = Client.get_client_by_phone_no(fineract.request_handler, phone_no)
     assert bool(client) == result
 
 
-def test_get_client_loans(fineract: Fineract):
+def test_get_client_loans(fineract):
     client = fineract.get_client(8)
     assert client
     loans = [loan for loan in client.get_loans()]
