9c332d78b95f4eb7c82866243f0d28bb665d282b
Michael Bukachi <michaelbukachi@gmail.com>
2019-04-07 22:31:00 +0300

    test: Add integration tests for Staff object

--- tests/integration/test_org.py
+++ tests/integration/test_org.py
@@ -7,3 +7,14 @@ def test_get_single_office(fineract):
     office = fineract.get_offices(2)
     assert office
     assert office.name == 'Merida'
+
+
+def test_get_all_staff(fineract):
+    staff = [staff for staff in fineract.get_staff()]
+    assert len(staff) == 3
+
+
+def test_get_single_staff(fineract):
+    staff = fineract.get_staff(2)
+    assert staff
+    assert staff.display_name == 'M, Mary'
