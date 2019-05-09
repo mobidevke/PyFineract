4b6422b2361ba333b9f46021d50e6f32fa7d9b52
Michael Bukachi <michaelbukachi@gmail.com>
2019-05-09 14:27:03 +0300

    feat: New pagination implementation
    This commit adds a new pagination implementation

--- fineract/pagination.py
+++ fineract/pagination.py
@@ -160,3 +160,80 @@ class PaginatedList(PaginatedListBase):
             self.__klass(self.__request_handler, element, False)
             for element in data
         ]
+
+
+class PaginatedList2:
+    """This class represents a paginated list of items fetched from a Fineract API
+
+
+    :param klass: Child class of a :class:`fineract.objects.fineract_object.FineractObject`
+    :param request_handler: :class:`fineract.handlers.RequestHandler`
+    :param url: Endpoint to be called
+    :param params: dict Extra payload to pass during the call
+    :param list_item: Response field holding the items
+    """
+    def __init__(self, klass, request_handler, url, params, list_item='pageItems'):
+        self._request_handler = request_handler
+        self._klass = klass
+        self._url = url
+        self._params = params or {'offset': 0}
+        self._params = params or {}
+        if 'offset' not in self._params:
+            self._params['offset'] = 0
+        self._params['limit'] = self._request_handler.per_page
+        self._list_item = list_item
+        self._total_count = None
+        self._no_pagination = None
+        self._elements = list()
+        self._load_data()
+
+    def _load_data(self):
+        data = self._request_handler.make_request(
+            'GET',
+            self._url,
+            params=self._params
+        )
+        data = data if data else {}
+        if isinstance(data, dict):
+            if self._list_item in data:
+                self.__total_count = data.get('totalFilteredRecords')
+                data = data[self._list_item]
+            else:
+                data = []
+        else:
+            self._no_pagination = True
+
+        self._elements = [
+            self._klass(self._request_handler, element, False)
+            for element in data if element is not None
+        ]
+
+    @property
+    def total_count(self):
+        """Returns total count of items"""
+        if self._total_count is None:
+            self._total_count = len(self._elements)
+
+        return self._total_count
+
+    def get_next(self):
+        """Load next page of items"""
+        self._params['offset'] = (self._params['offset'] + 1) * self._params['limit']
+        self._load_data()
+
+    def has_next(self):
+        """Check if there is another page
+
+        :rtype: bool
+        """
+        if self._no_pagination:
+            return False
+
+        return any(self._elements)
+
+    def __iter__(self):
+        for element in self._elements:
+            yield element
+
+    def __getitem__(self, item):
+        return self._elements[item]
