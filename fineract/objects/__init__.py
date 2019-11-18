273c9e924f665bc4f58382f2b9cbb9cbabdefc03
Michael Bukachi <michaelbukachi@gmail.com>
2019-11-18 22:48:08 +0300

    feat: Add savings object

--- fineract/objects/__init__.py
+++ fineract/objects/__init__.py
@@ -5,5 +5,6 @@ from .hook import Hook
 from .loan import Loan
 from .report import Report
 from .note import Note
+from .savings import Savings
 
-__all__ = ['Client', 'DataTable', 'Document', 'Report', 'Hook', 'Loan', 'Note']
+__all__ = ['Client', 'DataTable', 'Document', 'Report', 'Hook', 'Loan', 'Note', 'Savings']
